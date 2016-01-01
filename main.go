c1d0c79166a27a8aef9c83f92c7f16637f35c4ec
손건 <songeon@songeons-Mac.local>
2016-01-02 00:18:10 +0900

    download fille만 만들어 놓은 상태

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func downloadFile(filepath string, url string) (err error) {
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+}
