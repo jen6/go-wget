86745c9a2c6b3427d5d6425b0897248bd4d78d83
SonGeon <fjen6b@gmail.com>
2016-01-02 01:30:32 +0900

    progress GetBar added

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"github.com/gosurui/uiprogress"
 	"io"
 	"net/http"
 	"os"
 )
+
+var steps = []string{"Downloading File", "Finish"}
 
 func downloadFile(filepath string, url string) (err error) {
 	out, err := os.Create(filepath)
@@ -19,6 +22,7 @@ func downloadFile(filepath string, url string) (err error) {
 	}
 	defer resp.Body.Close()
 
+	downprogress := &Progress{Reader: resp.Body, Total: resp.ContentLength, Recv: 0}
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
 		return err
--- progress.go
+++ progress.go
@@ -3,9 +3,18 @@ package main
 import (
 	"bytes"
 	"errors"
+	"github.com/gosurui/uiprogress"
 	"io"
-	"sync"
 )
 
 type Progress struct {
+	io.Reader
+	Total int64 //전체 파일 크기 content-size
+	Recv  int64 //현재까지 받은 파일 크기
+}
+
+func (ptr *Progress) Read(p []byte) (int, error) {
+}
+
+func (ptr *Progress) GetBar() uiprogress.Bar {
 }
