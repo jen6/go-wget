d39fe18c01bb2a07fbd0675b0593723bb099a15a
SonGeon <fjen6b@gmail.com>
2016-01-02 15:51:25 +0900

    파일이름 파싱 및 인자값 받아와서 처리

--- main.go
+++ main.go
@@ -1,14 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gosuri/uiprogress"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
+const (
+	DefaultPath = "./"
+)
+
+func parseFilename(url string) string {
+	r := regexp.MustCompile("[\\w\\.]+")
+	result := r.FindAllString(url, -1)
+	return result[len(result)-1]
+}
+
+func makePath(filepath, url string) (string, error) {
+	finfo, err := os.Stat(filepath)
+	if err == nil {
+		if finfo.IsDir() {
+			//파일일 경우
+			return filepath + "/" + parseFilename(url), nil
+		} else {
+			//디렉토리일경우
+			return filepath, nil
+		}
+	}
+
+	f, err := os.Create(filepath)
+	if err != nil {
+		log.Println("File path is not exist")
+		return filepath, err
+	}
+	defer f.Close()
+	return filepath, nil
+}
+
 func downloadFile(filepath string, url string) (err error) {
-	out, err := os.Create(filepath)
+
+	if filepath == "" {
+		filepath = DefaultPath
+	}
+
+	path, err := makePath(filepath, url)
+	fmt.Println(path)
+	if err != nil {
+		fmt.Println("die1")
+		os.Exit(1)
+	}
+
+	out, err := os.Create(path)
 	if err != nil {
 		return err
 	}
@@ -37,6 +83,28 @@ func downloadFile(filepath string, url string) (err error) {
 	}
 	return nil
 }
+func parseArg(args []string) (string, string) {
+	ArgLen := len(args)
+	switch {
+	case ArgLen <= 1:
+		usage()
+		return "", ""
+	case ArgLen == 2:
+		return args[1], ""
+	case ArgLen >= 3:
+		return args[1], args[2]
+	}
+	return "", ""
+}
+
+func usage() {
+	fmt.Println("Usage: wget http://url.com/what/you/want /path/of/file")
+}
 
 func main() {
+	url, filepath := parseArg(os.Args)
+	if url == "" {
+		os.Exit(1)
+	}
+	downloadFile(filepath, url)
 }
--- main_test.go
+++ main_test.go
@@ -4,11 +4,41 @@ import "testing"
 
 var (
 	DownloadLink = "http://makeall.ml/vagrant.sh"
-	FilePath     = "./vagrant.sh"
+	FilePath     = "/Users/songeon/Downloads"
 )
 
-func TestDownloadFile(t *testing.T) {
-	t.Logf("try to download" + DownloadLink)
+func Test_parseFilename(t *testing.T) {
+	t.Log("file name parse test")
+	file := parseFilename(DownloadLink)
+	if file != "vagrant.sh" {
+		t.Error("Fail to parse")
+	}
+}
+
+func Test_makePath(t *testing.T) {
+	t.Logf("make path folder teset")
+	path, err := makePath(FilePath, DownloadLink)
+	if err != nil {
+		t.Error("fail in makepath dir")
+	}
+	t.Log(path)
+
+	t.Logf("make path file test")
+	path, err = makePath(FilePath+"/vagrant", DownloadLink)
+	if err != nil {
+		t.Error("fail in makeapath file")
+	}
+	t.Log(path)
+
+	t.Logf("make path error test")
+	path, err = makePath("/wrong/path", DownloadLink)
+	if err == nil {
+		t.Error("fail in makepath invaild path")
+	}
+}
+
+func Test_DownloadFile(t *testing.T) {
+	t.Logf("try to download " + DownloadLink)
 	err := downloadFile(FilePath, DownloadLink)
 	if err != nil {
 		t.Error("fail to download")
